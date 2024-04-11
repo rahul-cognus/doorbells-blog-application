fb9c1820b3fd890a7d14d27bd7d71c71d6b1dd9d
Rahul Jangid <rahuljangid236@gmail.com>
2024-04-12 00:23:10 +0530

    Changes

--- admin/src/ui/Layout/Footer.jsx
+++ admin/src/ui/Layout/Footer.jsx
@@ -2,21 +2,21 @@ import React from 'react'
 
 const Footer = () => {
     return (
-        <footer class="mb-3">
-            <div class="container">
-                <div class="card card-body bg-light">
-                    <div class="row align-items-center justify-content-between">
-                        <div class="col-lg-6">
+        <footer className="mb-3">
+            <div className="container">
+                <div className="card card-body bg-light">
+                    <div className="row align-items-center justify-content-between">
+                        <div className="col-lg-6">
                             {/* Copyright */}
-                            <div class="text-center text-lg-start">©2023 <a href="https://www.webestica.com/" class="text-reset btn-link" target="_blank">Webestica</a>. All rights reserved
+                            <div className="text-center text-lg-start">©2023 <a href="https://www.webestica.com/" className="text-reset btn-link" target="_blank">Webestica</a>. All rights reserved
                             </div>
                         </div>
-                        <div class="col-lg-6 d-sm-flex align-items-center justify-content-center justify-content-lg-end">
+                        <div className="col-lg-6 d-sm-flex align-items-center justify-content-center justify-content-lg-end">
                             {/* Links */}
-                            <ul class="nav text-center text-sm-end justify-content-center justify-content-center mt-3 mt-md-0">
-                                <li class="nav-item"><a class="nav-link" href="#">Terms</a></li>
-                                <li class="nav-item"><a class="nav-link" href="#">Privacy</a></li>
-                                <li class="nav-item"><a class="nav-link pe-0" href="#">Cookies</a></li>
+                            <ul className="nav text-center text-sm-end justify-content-center justify-content-center mt-3 mt-md-0">
+                                <li className="nav-item"><a className="nav-link" href="#">Terms</a></li>
+                                <li className="nav-item"><a className="nav-link" href="#">Privacy</a></li>
+                                <li className="nav-item"><a className="nav-link pe-0" href="#">Cookies</a></li>
                             </ul>
                         </div>
                     </div>
