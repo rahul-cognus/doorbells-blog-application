a14aac57b0e1e042f96c1bb86ea58b4c16d63f6d
Rahul Jangid <rahul.jangid.cognus@gmail.com>
2024-04-12 14:46:27 +0530

    Changes

--- admin/src/app/layout.js
+++ admin/src/app/layout.js
@@ -1,6 +1,8 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { ApolloWrapper } from "@/lib/apollo-wrapper";
+import { Provider } from 'react-redux';
+import store from '@/redux/store'
 
 import Header from "@/ui/Layout/Header";
 import Footer from "@/ui/Layout/Footer";
@@ -20,11 +22,13 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        <ApolloWrapper>
-          <Header />
-          {children}
-          <Footer />
-        </ApolloWrapper>
+        <Provider store={store}>
+          <ApolloWrapper>
+            <Header />
+            {children}
+            <Footer />
+          </ApolloWrapper>
+        </Provider>
       </body>
     </html>
   );
---
+++ admin/src/redux/stateReducer.js
@@ -0,0 +1,10 @@
+
+import { combineReducers } from 'redux';
+
+// Define a reducer function
+const rootReducer = (state = initialState, action) => {
+    // Handle actions here if needed
+    return state;
+  };
+
+export default rootReducer;
